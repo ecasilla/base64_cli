2ae50c521c36e2344950861e7276b420f554d293
Ernie Casilla <ecasilla@icloud.com>
2015-10-06 17:35:25 -0400

    added the gulp deps

--- tasks/tasks/watch.js
+++ tasks/tasks/watch.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp'),
-    config = require('../config')
+    config = require('../config'),
+    log = require('../util/gulpLogger');
 
 gulp.task('watch', function () {
     gulp.watch(config.lint.all, ['build']);
     gulp.watch(config.spec.path,['test:unit']);
+    log.watch(config.lint.all)
+    log.watch(config.spec.path)
 });
